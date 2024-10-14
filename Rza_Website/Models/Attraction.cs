6bcec6f869c717de62ffa63d045d275085601804
Abdoul Diallo <abdoulmdiallo416@gmail.com>
2024-10-14 12:12:15 +0100

    Add project files.

---
+++ Rza_Website/Models/Attraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rza_Website.Models;
+
+public partial class Attraction
+{
+    public int AttractioniD { get; set; }
+
+    public string? AttractionName { get; set; }
+
+    public int Capacity { get; set; }
+
+    public DateTime? Schedule { get; set; }
+
+    public float? Price { get; set; }
+
+    public string? SpecialReqs { get; set; }
+
+    public int? TicketiD { get; set; }
+
+    public virtual Ticket? Ticket { get; set; }
+}
---
+++ Rza_Website/Models/Ticket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rza_Website.Models;
+
+public partial class Ticket
+{
+    public int? CustomeriD { get; set; }
+
+    public int? AttractioniD { get; set; }
+
+    public int TicketiD { get; set; }
+
+    public float? Price { get; set; }
+
+    public DateOnly? ValidFrom { get; set; }
+
+    public string? TicketType { get; set; }
+
+    public virtual Customer? Attraction { get; set; }
+
+    public virtual ICollection<Attraction> Attractions { get; set; } = new List<Attraction>();
+}
