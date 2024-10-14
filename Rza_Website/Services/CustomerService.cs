90a2fe379055d5e81388ec08686b574fc0c20371
Abdoul Diallo <abdoulmdiallo416@gmail.com>
2024-10-14 12:25:08 +0100

    added new folder called services and a C# file called customer servioce imported entity frameworka and Models and added these lines of code:
    "private readonly  TlS2300852RzaContext _context;
    public CustomerService(TlS2300852RzaContext context)
    {
        _context = context;}"

---
+++ Rza_Website/Services/CustomerService.cs
@@ -0,0 +1,14 @@
+﻿using Rza_Website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rza_Website.Services
+{
+    public class CustomerService
+    {
+        private readonly  TlS2300852RzaContext _context;
+        public CustomerService(TlS2300852RzaContext context)
+        {
+            _context = context;
+        }
+    }
+}
