00bf7efce0949120ef627e66cfcfc90a367723e2
Abdoul Diallo <abdoulmdiallo416@gmail.com>
2024-11-05 09:25:11 +0000

    added unit testing

---
+++ UnitTest_Demo/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Rza_Website.Services;
+using Rza_Website.Components;
+using Rza_Website.Utilities;
+using Rza_Website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTest_Demo
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+            var options = new DbContextOptionsBuilder<TlS2300852RzaContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+                _context = new TlS2300852RzaContext(options);
+            _customerService = new CustomerService(_context);
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
