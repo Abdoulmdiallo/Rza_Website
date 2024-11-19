a248a7bcea734cf71369fee62cbcd01826fff6f2
Abdoul Diallo <abdoulmdiallo416@gmail.com>
2024-11-19 09:56:09 +0000

    added ticket bookings to the bookings page

--- Rza_Website/Services/TicketBookingService.cs
+++ Rza_Website/Services/TicketBookingService.cs
@@ -1,6 +1,24 @@
-﻿namespace Rza_Website.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Rza_Website.Models;
+
+
+namespace Rza_Website.Services
 {
     public class TicketBookingService
     {
+        private readonly TlS2300852RzaContext _context;
+        public TicketBookingService(TlS2300852RzaContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Ticket>> GetTicketsAsync()
+        {
+            return await _context.Tickets.ToListAsync();
+        }
+        public async Task AddTicketAsync(Ticket newTicket)
+        {
+            await _context.Tickets.AddAsync(newTicket);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- Rza_Website/Services/TicketService.cs
+++ Rza_Website/Services/TicketService.cs
@@ -1,6 +1,42 @@
-﻿namespace Rza_Website.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Rza_Website.Models;
+
+namespace Rza_Website.Services
+
 {
+
     public class TicketService
+
     {
+
+        private readonly TlS2300852RzaContext _context;
+
+        public TicketService(TlS2300852RzaContext context)
+
+        {
+
+            _context = context;
+
+        }
+
+        public async Task<List<Ticket>> GetTicketsAsync()
+
+        {
+
+            return await _context.Tickets.ToListAsync();
+
+        }
+
+        public async Task AddTicketAsync(Ticket newTicket)
+
+        {
+
+            await _context.Tickets.AddAsync(newTicket);
+
+            await _context.SaveChangesAsync();
+
+        }
+
     }
+
 }
